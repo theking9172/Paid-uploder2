2e8be12a823ed5db897eb16af300ea85ddafeb83
theking9172 <sumittheking02@gmail.com>
2025-07-29 14:49:26 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,12 +1,12 @@
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28329455"))
-API_HASH = environ.get("API_HASH", "9de39a5d0a350cacfceb3e1bace9a40d")
+API_ID = int(environ.get("API_ID", "29435108"))
+API_HASH = environ.get("API_HASH", "2d211eb63606dae1bcb413d57391b2de")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Sudo Users Configuration
-SUDO_USERS = list(map(int, environ.get("SUDO_USERS", "7526345865").split(","))) if environ.get("SUDO_USERS", "") else []
+SUDO_USERS = list(map(int, environ.get("SUDO_USERS", "6050965589").split(","))) if environ.get("SUDO_USERS", "") else []
 
 # Admin Configuration - Has FULL and PERMANENT access
-ADMIN_USERS = list(map(int, environ.get("ADMIN_USERS", "7611019705").split(","))) if environ.get("ADMIN_USERS", "") else []
+ADMIN_USERS = list(map(int, environ.get("ADMIN_USERS", "6050965589").split(","))) if environ.get("ADMIN_USERS", "") else []
